6e8150ac67fe2eafcff1edf2f7be8b79a8bdc4d5
PVRSaiKumar <99071532+PVRSaiKumar@users.noreply.github.com>
2022-11-21 17:18:40 +0530

    Add files via upload

--- client.py
+++ client.py
@@ -1,37 +1,151 @@
+from pickle import FALSE, GLOBAL
+from re import A
 import socket
-import select
-import sys
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+import threading
+from traceback import print_tb
+import colorama
+from colorama import Fore
+from sa import *
+import os
 
-IP_address = str(sys.argv[1]) 
-Port = int(sys.argv[2]) 
-server.connect((IP_address, Port)) 
-user_name=str(sys.argv[3])# user name for identifying admin without white spaces
-password=str(sys.argv[4])#  password of the user without white spaces
-file1=open("1user.txt","a")
-file1.write(user_name+" "+password)
-file1.close()
-while True:
-	# maintains a list of possible input streams 
-	sockets_list = [sys.stdin, server]
-	""" There are two possible input situations. Either the 
-	user wants to give manual input to send to other people, 
-	or the server is sending a message to be printed on the 
-	screen. Select returns from sockets_list, the stream that 
-	is reader for input. So for example, if the server wants 
-	to send a message, then the if condition will hold true 
-	below.If the user wants to send a message, the else 
-	condition will evaluate as true"""
+SERVER_ADDRESS = ('localhost', 7889)
+# SERVER_ADDRESS1 = ('127.0.0.1', 15661)
+# SERVER_ADDRESS2 = ('127.0.0.1', 15662)
+# SERVER_ADDRESS3 = ('127.0.0.1', 15663)
+# SERVER_ADDRESS4 = ('127.0.0.1', 15664)
+ENCODING = 'utf-8'
 
-	read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
-	for socks in read_sockets: 
-		if socks == server: 
-			message = socks.recv(2048) 
-			print (message) 
-		else: 
-			message = sys.stdin.readline() 
-			server.send(message.encode()) 
-			sys.stdout.write("<You>")
-			sys.stdout.write(message)
-			sys.stdout.flush()
-server.close()
+
+def receive_from(client_socket, decode_type:str =ENCODING, size: int = 1024):
+	if decode_type=="":
+		return client_socket.recv(size).decode()
+	else:
+ 		return client_socket.recv(size).decode(decode_type)
+
+
+def Send(client_socket, message :str,encod_type:str =ENCODING):
+	if encod_type=="":
+		return client_socket.send(message.encode())
+	elif encod_type=="NONE":
+		return client_socket.send(message)
+	else:
+ 		return client_socket.send(message.encode(ENCODING))
+
+
+def Send_msg(client_socket, message :str,publickey:int):
+	Send(client_socket,encrypt(message,publickey),"NONE")
+
+
+commands=["/Send",'/Exit','/Creategrp','/Addmember','/Sendgrp','/kick']
+def handle_server_instruction(message):
+	if len(message)<2:
+		print(Fore.WHITE+"Received unknown instruction "+message+" from server")
+	if message[0]!="%" or message[len(message)-1]!="%":
+		print(Fore.WHITE+"Received unknown instruction "+message+" from server")
+
+	if message == '%USER%':
+		Send(client_socket,username)
+	
+	elif message == '%PASS%':
+		Send(client_socket,password)
+
+	elif message=='%CONNECT%':
+		threading.Thread(target=write).start()
+	
+	elif message == '%CREATEGROUP%':
+		groupname=input(Fore.GREEN+'GROUP NAME:\n'+Fore.RED+"$ "+Fore.BLUE)
+		Send(client_socket,groupname)
+		threading.Thread(target=write).start()
+
+	elif message=="%SENDGRP%":
+		groupname=input(Fore.GREEN+"GROUPNAME:\n"+Fore.RED+"$ "+Fore.BLUE)
+		Message=input(Fore.GREEN+"MESSAGE:\n"+Fore.RED+"$ "+Fore.BLUE)
+		Send(client_socket,groupname)
+		Send(client_socket,Message)
+		print(Fore.WHITE+receive_from(client_socket))
+		threading.Thread(target=write).start()
+
+	elif message=="%ADDMEMBER%":
+		groupname=input(Fore.GREEN+'GROUP NAME:\n'+Fore.RED+"$ "+Fore.BLUE)
+		member=input(Fore.GREEN+'NAME OF THE PERSON TO BE ADDED:\n'+Fore.RED+"$ "+Fore.BLUE)
+		Send(client_socket,groupname)
+		Send(client_socket,member)
+		print(Fore.WHITE+receive_from(client_socket))
+		threading.Thread(target=write).start()
+	
+	elif message == '%TRYAGAIN%':
+		password2=input(Fore.RED+'TRYAGAIN\n'+Fore.GREEN+"Password: "+Fore.WHITE)
+		Send(client_socket,password2)
+	
+	elif message == '%QUIT%':
+		print(Fore.RED+'Exiting...')
+		client_socket.close()
+		exit(0)
+	
+	elif message =="%DRT_MSG%":
+		username_frnd = input(Fore.GREEN+"User name to which message is to be sent: \n"+Fore.RED+"$ "+Fore.BLUE)
+		message = input(Fore.GREEN+"Message: \n"+Fore.RED+"$ "+Fore.BLUE)
+		Send(client_socket,username_frnd)
+		Send(client_socket,message)
+		threading.Thread(target=write).start()
+	
+	elif message=="%REMOVE%":
+		username_frnd = input(Fore.GREEN+"User name to be removed: \n"+Fore.RED+"$ "+Fore.BLUE)
+		grp_name=input(Fore.GREEN+"From which group user to be removed: \n"+Fore.RED+"$ "+Fore.BLUE)
+		Send(client_socket,username_frnd)
+		Send(client_socket,grp_name)
+		threading.Thread(target=write).start()
+	else:
+		print(Fore.WHITE+"Received unknown instruction "+message+" from server\n"+Fore.RED+"$ ")
+
+
+def receive():
+	z=0
+	"""Pool messages from server (runs in separate thread)"""
+	while True:
+		try:
+			message = client_socket.recv(1024).decode(ENCODING)
+			if message.startswith('%'):
+				handle_server_instruction(str(message))
+			else:
+				msg=message.split(":",1)
+				if z==0:
+					print("hi")
+					#print(Fore.WHITE+msg[0]+": "+decrypt(msg[1],privatekey))
+				else:
+					print(Fore.WHITE+message+Fore.RED+"\n$ ",end='')
+				z+=1
+		except OSError:
+			print('An error occurred!')
+			client_socket.close()
+			break
+
+
+def write():
+	"""Hold open input for sending messages (runs in separate thread)"""
+	while True:
+		message=input(Fore.RED+"$ ")
+		if message in commands:
+			Send(client_socket,message)
+			break   
+
+
+if __name__ == '__main__':
+	try:
+		username = input(Fore.GREEN+"Username: "+Fore.WHITE)
+		password = input(Fore.GREEN+"Password: "+Fore.WHITE)
+		client_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		# client_sockets=[]
+		# for i in range(5):
+		# 	client_sockets.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+		client_socket.connect(SERVER_ADDRESS)
+		# client_sockets[0].connect(SERVER_ADDRESS)
+		# client_sockets[0].connect(SERVER_ADDRESS1)
+		# client_sockets[0].connect(SERVER_ADDRESS2)
+		# client_sockets[0].connect(SERVER_ADDRESS3)
+		# client_sockets[0].connect(SERVER_ADDRESS4)
+	except ConnectionRefusedError:
+		print(Fore.WHITE+'Could not connect to the server. Exiting...')
+		exit(1)
+	receive_thread = threading.Thread(target=receive)
+	receive_thread.start()
